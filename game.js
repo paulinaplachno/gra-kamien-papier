8566cadf94040629b28018f5637260d7f3331f3e
Paulina Płachno <paulina1plachno@gmail.comgit config -- global user.namecleargit configgit config --global user.email paulina1plachno@gmail.com>
2022-05-26 10:00:50 +0200

    wyodrebiony plik style.css

--- game.js
+++ game.js
@@ -17,7 +17,7 @@ function handSelection() {
     game.playerHand = this.dataset.option
     console.log(game.playerHand);
     hands.forEach( hand => hand.style.boxShadow = "");
-    this.style.boxShadow = "0 0 0 4px green"
+    this.style.boxShadow = "0 0 0 6px black"
 }
 
 function aiChoice() {
@@ -42,15 +42,15 @@ document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
 
 if (result === "win") {
     document.querySelector('p.wins span').textContent = ++gameSummary.win;
-    document.querySelector('[data-summary="who-win"]').textContent = "Ty wygrałeś!!!!"
+    document.querySelector('[data-summary="who-win"]').textContent = "Wygrałeś!"
     document.querySelector('[data-summary="who-win"]').style.color = "green";
    } else if (result === "loss") {
     document.querySelector('p.losses span').textContent = ++gameSummary.losses;
-    document.querySelector('[data-summary="who-win"]').textContent = "Komputer wygrał :("
+    document.querySelector('[data-summary="who-win"]').textContent = "Wygrał komputer :("
     document.querySelector('[data-summary="who-win"]').style.color = "red";
    } else {
     document.querySelector('p.draws span').textContent = ++gameSummary.draws;
-    document.querySelector('[data-summary="who-win"]').textContent = "Remis :\\"
+    document.querySelector('[data-summary="who-win"]').textContent = "Remis!"
     document.querySelector('[data-summary="who-win"]').style.color = "gray";
    }
 }
